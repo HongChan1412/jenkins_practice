0ae613002a9db2672610a3c09318d99c5d074d38
nahong_c <95984922+HongChan1412@users.noreply.github.com>
2025-04-09 11:38:41 +0900

    test : test 오류 수정 디버깅

--- step07_CICD/src/test/java/com/example/demo/Step07CicdApplicationTests.java
+++ step07_CICD/src/test/java/com/example/demo/Step07CicdApplicationTests.java
@@ -12,16 +12,9 @@ class Step07CicdApplicationTests {
     @Test
     public void testAddition() {
         int result = 12 + 3;
-        /* 예시 - 서버 시작시에 특정 DATABASE로부터 검색
-         * 그 데이터는 꼭 fisa여야만 하는 상황
-         * fisa가 없다면 서버 중지 또는 다른 로직 실행
-         * 
-         * 개발 중에 데이터에 대한 test 검증용등으로 사용
-         * 
-         */
         
         System.out.println("비교 전 ***********");
-        assertEquals(15, result);  // 성공: 5 == 5
+        assertEquals(15, result);  
         System.out.println("비교 후 ***********");
     }
 
