1c2bb073a5ab9909f96d4eb9a8d0697d8721bdfb
nahong_c <evangelion14125@gmail.com>
2025-04-09 10:45:14 +0900

    test : jenkins webhook test

--- step07_CICD/src/test/java/com/example/demo/Step07CicdApplicationTests.java
+++ step07_CICD/src/test/java/com/example/demo/Step07CicdApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Step07CicdApplicationTests {
     @Test
     public void testAddition() {
-        int result = 2 + 3;
+        int result = 12 + 3;
         /* 예시 - 서버 시작시에 특정 DATABASE로부터 검색
          * 그 데이터는 꼭 fisa여야만 하는 상황
          * fisa가 없다면 서버 중지 또는 다른 로직 실행
@@ -21,7 +21,7 @@ class Step07CicdApplicationTests {
          */
         
         System.out.println("비교 전 ***********");
-        assertEquals(5, result);  // 성공: 5 == 5
+        assertEquals(15, result);  // 성공: 5 == 5
         System.out.println("비교 후 ***********");
     }
 
