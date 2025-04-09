bd0561051d9e8533d6ee4a7d781e96f429031737
nahong_c <evangelion14125@gmail.com>
2025-04-09 09:45:27 +0900

    feat : test 추가

--- step07_CICD/src/main/java/edu/fis/ce/CICDController.java
+++ step07_CICD/src/main/java/edu/fis/ce/CICDController.java
@@ -1,12 +1,20 @@
 package edu.fis.ce;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.java.Log;
+
 @RestController
+@Slf4j
 public class CICDController {
 	@GetMapping("fisa1")
 	public String reqRes() {
 		System.out.println("reqRES************");
-		
+		log.info("*** 요청 & 응답 ***");
+		for (int i = 1; i <= 10; i ++) {
+			System.out.println("data 값" + i	);
+		}
 		return "success";
 	}
 }
